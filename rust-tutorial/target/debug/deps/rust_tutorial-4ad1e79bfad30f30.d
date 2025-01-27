a46eb8c35abe02f406f02f98f78187790181d1f5
lmoore36 <lmoore36@unc.edu>
2025-01-27 00:26:58 +0000

    Following my partner's rust tutorial.

---
+++ rust-tutorial/target/debug/deps/rust_tutorial-4ad1e79bfad30f30.d
@@ -0,0 +1,5 @@
+/workspaces/rust-tutorial-project/rust-tutorial/target/debug/deps/rust_tutorial-4ad1e79bfad30f30: src/main.rs
+
+/workspaces/rust-tutorial-project/rust-tutorial/target/debug/deps/rust_tutorial-4ad1e79bfad30f30.d: src/main.rs
+
+src/main.rs:
---
+++ rust-tutorial/target/debug/rust-tutorial.d
@@ -0,0 +1,1 @@
+/workspaces/rust-tutorial-project/rust-tutorial/target/debug/rust-tutorial: /workspaces/rust-tutorial-project/rust-tutorial/src/main.rs
